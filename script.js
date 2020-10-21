c8906117da6e113b00eaee694e925b4e9ce7f7a6
Jmtri7 <Jmtri7@gmail.com>
2020-10-21 13:39:36 -0400

    reformatting posts

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
 posts = [
 
 {
-    "subject": "My First Post",
-    "message": "This is my first post."
+    "subject" : "My First Post",
+    "date" : "10/20/2020",
+    "message" : "This is my first post."
 },
 
 {
-    "subject": "My Second Post",
-    "message": "This is my second post."
+    "subject" : "My Middle Post",
+    "date" : "10/20/2020",
+    "message" : "This is my second post."
+},
+
+
+{
+    "subject" : "My Latest Post",
+    "date" : "10/20/2020",
+    "message" : "This is my second post."
 },
 
 ]
 
-var postDisplays = document.getElementsByClassName("postDisplay");
-
-console.log(postDisplays[1]);
-
+postDisplays = document.getElementsByClassName("postDisplay");
 var post = document.createElement('DIV');
-for (i = 0; i < posts.length; i++) {
-  post.innerHTML = '<div>' + posts[i].subject + '<br>' + posts[i].message + '<br><br></div>';
-  postDisplays[0].appendChild(post.firstChild);
+for (i = posts.length - 1; i >= 0; i--) {
+
+  post.innerHTML = 
+  '<div class="post">' 
+  + posts[i].subject 
+  + '<br>' 
+  + posts[i].date 
+  + '<br>' 
+  + posts[i].message 
+  + '<br><br></div>';
+
+	for (j = 0; j < postDisplays.length; j++) {
+  		postDisplays[j].innerHTML += post.innerHTML;
+	}
 }
 
 
